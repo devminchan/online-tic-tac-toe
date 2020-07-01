e2630671fc9f9d305790949a2b7a4862eb851569
devminchan <minchan.dev@gmail.com>
2020-07-01 22:52:27 +0900

    채팅 레이아웃 완성

--- src/App.js
+++ src/App.js
@@ -21,13 +21,32 @@ function App() {
           </div>
 
           {/* chatting */}
-          <div className="flex-basis-1">
-            <ul className="px-4 py-2">
-              <li>
-                <div>username</div>
+          <div className="flex-basis-1 px-4 py-2 flex flex-col">
+            <ul className="flex-grow h-auto flex flex-col-reverse">
+              <li className="w-full mx-2 flex flex-wrap">
+                <div className="mr-4 text-base font-semibold">username</div>
                 <div>hello</div>
               </li>
+              <li className="w-full mx-2 flex flex-wrap">
+                <div className="mr-4 text-base font-semibold">username</div>
+                <div>hi</div>
+              </li>
             </ul>
+            <div className="mt-2 h-12">
+              <form action="" className="w-full h-full flex items-center">
+                <input
+                  className="flex-grow h-8 px-2 rounded"
+                  type="text"
+                  placeholder="Input Text!"
+                />
+                <button
+                  className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-1 px-4 ml-4 rounded text-base"
+                  type="submit"
+                >
+                  전송
+                </button>
+              </form>
+            </div>
           </div>
         </div>
         {/* right-side container */}
@@ -35,22 +54,10 @@ function App() {
           {/* leaderboard */}
           <div>
             <ul className="">
-              <li>
+              <li className="">
                 <div>1</div>
                 <div>username</div>
                 <div>score</div>
-              </li>
-            </ul>
-          </div>
-          {/* history */}
-          <div>
-            <ul>
-              <li>
-                <div>2020-07-01</div>
-                <div>username</div>
-                <div>vs</div>
-                <div>username2</div>
-                <div>LOSE</div>
               </li>
             </ul>
           </div>
