393cef64a4cf31ff9249ea150dfa7ae7cf227653
devminchan <minchan.dev@gmail.com>
2020-07-02 00:22:39 +0900

    overflow 관련 문제 해결 & 헤더 제거

--- src/App.js
+++ src/App.js
@@ -1,36 +1,61 @@
 import React from 'react';
 
 function App() {
+  const rankingList = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    12,
+    13,
+    14,
+    15,
+  ];
+
   return (
-    <div className="App flex flex-col justify-between h-screen">
-      <div className="w-full flex justify-center pt-4 pb-2">
-        <h1 className="text-5xl text-purple-600 font-sans">
-          Online Tic-Tac-Toe
-        </h1>
-      </div>
+    <div className="App justify-between h-screen">
       {/* body */}
       <div className="w-screen h-full bg-gray-300 flex">
         {/* left-side container */}
-        <div className="w-7/12 h-full flex flex-col">
+        <div className="w-7/12 block flex flex-col">
           {/* match button */}
-          <div className="flex-basis-2 flex flex-col justify-center items-center">
-            <button className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded text-4xl">
-              PLAY NOW!
-            </button>
-            <div className="pt-3 text-2xl text-gray-700">0:00</div>
+          <div className="w-full flex-grow relative">
+            <div className="w-full h-full flex flex-col justify-center items-center">
+              <p className="absolute top-0 mt-4 text-xl text-purple-500">
+                welcome to online tic-tac-toe!
+              </p>
+              <button className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded text-4xl">
+                PLAY NOW!
+              </button>
+              <div className="pt-3 text-2xl text-gray-700">0:00</div>
+            </div>
           </div>
 
           {/* chatting */}
-          <div className="flex-basis-1 px-4 py-2 flex flex-col">
-            <ul className="flex-grow h-auto flex flex-col-reverse">
-              <li className="w-full mx-2 flex flex-wrap">
-                <div className="mr-4 text-base font-semibold">username</div>
-                <div>hello</div>
-              </li>
-              <li className="w-full mx-2 flex flex-wrap">
-                <div className="mr-4 text-base font-semibold">username</div>
-                <div>hi</div>
-              </li>
+          <div className="w-full px-4 py-2 block">
+            <ul className="h-48 flex-grow-0 flex flex-col-reverse overflow-auto">
+              {rankingList.map((rank) => (
+                <li className="w-full mx-2 flex flex-wrap">
+                  <div className="mr-4 text-base font-semibold">username</div>
+                  <div>hello</div>
+                </li>
+              ))}
             </ul>
             <div className="mt-2 h-12">
               <form action="" className="w-full h-full flex items-center">
@@ -52,16 +77,20 @@ function App() {
         {/* right-side container */}
         <div className="w-5/12 pt-8 pb-4 pl-8 pr-8">
           {/* leaderboard */}
-          <div className="bg-gray-400 w-full h-full rounded">
-            <div className="flex justify-center pt-2 pb-4">
+          <div className="bg-gray-400 w-full h-full rounded flex flex-col">
+            <div className="flex-1 flex justify-center pt-2 pb-4">
               <h3 className="text-3xl text-purple-600">LEADERBOARDS</h3>
             </div>
-            <ul className="">
-              <li className="flex bg-gray-500 rounded ml-6 mr-6 pl-4 pr-4 pt-1 pb-1 text-xl">
-                <div>1</div>
-                <div className="ml-5">username</div>
-                <div className="ml-auto">score</div>
-              </li>
+            <ul className="h-128 overflow-y-auto">
+              {rankingList.map((rank) => (
+                <li className="block w-full">
+                  <div className="flex bg-gray-500 rounded ml-6 mr-6 pl-4 pr-4 pt-1 pb-1 text-xl mb-2">
+                    <div>{rank}</div>
+                    <div className="ml-5">username</div>
+                    <div className="ml-auto">score</div>
+                  </div>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
