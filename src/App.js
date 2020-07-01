0aa1fcffb38e5ad95a36b6d106297299c6bc4316
devminchan <minchan.dev@gmail.com>
2020-07-01 23:08:15 +0900

    리더보드 레이아웃 완성

--- src/App.js
+++ src/App.js
@@ -50,14 +50,17 @@ function App() {
           </div>
         </div>
         {/* right-side container */}
-        <div>
+        <div className="w-5/12 pt-8 pb-4 pl-8 pr-8">
           {/* leaderboard */}
-          <div>
+          <div className="bg-gray-400 w-full h-full rounded">
+            <div className="flex justify-center pt-2 pb-4">
+              <h3 className="text-3xl text-purple-600">LEADERBOARDS</h3>
+            </div>
             <ul className="">
-              <li className="">
+              <li className="flex bg-gray-500 rounded ml-6 mr-6 pl-4 pr-4 pt-1 pb-1 text-xl">
                 <div>1</div>
-                <div>username</div>
-                <div>score</div>
+                <div className="ml-5">username</div>
+                <div className="ml-auto">score</div>
               </li>
             </ul>
           </div>
