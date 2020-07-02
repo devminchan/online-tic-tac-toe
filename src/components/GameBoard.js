ab5d9980f75788f557b64e441fb73e92bcc14eff
devminchan <minchan.dev@gmail.com>
2020-07-02 14:03:02 +0900

    grid cell에 클릭시 이벤트 추가

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const userId = '1';
+const opponentId = '2';
 
 function GameBoard() {
+  const [mapState, setMapState] = useState(Array(100).fill(null));
+  const [turnCount, setTurnCount] = useState(0);
+
+  const handleBoardClick = (e) => {
+    const cellId = e.target.attributes.getNamedItem('cell-id').value;
+    let nowUserId = userId;
+
+    if (turnCount % 2 === 0) {
+      nowUserId = opponentId;
+    }
+
+    if (mapState[cellId]) {
+      return;
+    }
+
+    const newMapState = mapState.slice();
+
+    newMapState.splice(cellId, 1, {
+      userId: nowUserId,
+      turn: turnCount,
+    });
+
+    setMapState(newMapState);
+    setTurnCount(turnCount + 1);
+  };
+
   return (
     <div className="h-full w-full bg-gray-400 grid grid-cols-10 grid-rows-10 rounded">
       {[...Array(100).keys()].map((idx) => (
-        <div className="border"></div>
+        <div
+          cell-id={idx}
+          className="border flex justify-center items-center relative"
+          onClick={handleBoardClick}
+        >
+          {mapState[idx] ? (
+            <div className="absolute">{mapState[idx].userId}</div>
+          ) : (
+            ''
+          )}
+        </div>
       ))}
     </div>
   );
