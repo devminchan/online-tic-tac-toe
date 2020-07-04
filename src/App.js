d3e6c72df3a54a17d5b48dbded6922a0badf063f
devminchan <minchan.dev@gmail.com>
2020-07-04 13:53:38 +0900

    mark가 하나밖에 놓이지 않는 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,104 +1,160 @@
-import React, { useState } from 'react';
+import React from 'react';
 import GameBoard from './components/GameBoard';
+import * as Colyseus from 'colyseus.js';
+import { List } from 'immutable';
 
-function App() {
-  const rankingList = [1, 2, 3];
-
-  const [gameState, setGameState] = useState(false);
-
-  const handleGameStart = (e) => {
-    setGameState(true);
+class App extends React.Component {
+  state = {
+    gmaeState: false,
+    chatState: List([]),
   };
 
-  const handleGameStop = (e) => {
-    setGameState(false);
+  constructor() {
+    super();
+
+    // use current hostname/port as colyseus server endpoint
+    var endpoint = 'ws://localhost:2567';
+    this.client = new Colyseus.Client(endpoint);
+  }
+
+  setGameState = (gameState) => {
+    this.setState({
+      ...this.state,
+      gameState,
+    });
   };
 
-  return (
-    <div className="App justify-between h-screen">
-      {/* body */}
-      <div className="w-screen h-full bg-gray-300 flex">
-        {/* left-side container */}
-        <div className="w-7/12 block flex flex-col">
-          {/* match button & game board */}
-          <div className="w-full flex-grow relative">
-            {gameState ? (
-              /* game board */
-              <div className="w-full h-full flex flex-col justify-center items-center">
-                <p className="h-auto mt-4 text-xl text-purple-500">
-                  Username vs Username
-                </p>
-                <div className="w-2/3 mt-2 mb-2 h-full flex justify-center">
-                  <GameBoard onGameEnd={handleGameStop} />
-                </div>
-              </div>
-            ) : (
-              /* match button */
-              <div className="w-full h-full flex flex-col justify-center items-center">
-                <p className="absolute top-0 mt-4 text-xl text-purple-500">
-                  welcome to online tic-tac-toe!
-                </p>
-                <button
-                  className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded text-4xl"
-                  onClick={handleGameStart}
-                >
-                  PLAY NOW!
-                </button>
-                <div className="pt-3 text-2xl text-gray-700">0:00</div>
-              </div>
-            )}
-          </div>
+  setChatState = (chatState) => {
+    this.setState({
+      ...this.state,
+      chatState,
+    });
+  };
 
-          {/* chatting */}
-          <div className="w-full px-4 py-2 block">
-            <ul className="h-48 flex-grow-0 flex flex-col-reverse overflow-auto">
-              {rankingList.map((idx) => (
-                <li key={idx} className="w-full mx-2 flex flex-wrap">
-                  <div className="mr-4 text-base font-semibold">username</div>
-                  <div>hello</div>
-                </li>
-              ))}
-            </ul>
-            <div className="mt-2 h-12">
-              <form action="" className="w-full h-full flex items-center">
-                <input
-                  className="flex-grow h-8 px-2 rounded"
-                  type="text"
-                  placeholder="Input Text!"
-                />
-                <button
-                  className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-1 px-4 ml-4 rounded text-base"
-                  type="submit"
-                >
-                  전송
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-        {/* right-side container */}
-        <div className="w-5/12 pt-8 pb-4 pl-8 pr-8">
-          {/* leaderboard */}
-          <div className="bg-gray-400 w-full h-full rounded flex flex-col">
-            <div className="flex justify-center pt-2 pb-4">
-              <h3 className="text-3xl text-purple-600">LEADERBOARDS</h3>
-            </div>
-            <ul className="flex-grow overflow-y-auto">
-              {rankingList.map((idx) => (
-                <li key={idx} className="block w-full">
-                  <div className="flex bg-gray-500 rounded ml-6 mr-6 pl-4 pr-4 pt-1 pb-1 text-xl mb-2">
-                    <div className="w-3">{idx}</div>
-                    <div className="ml-5">username</div>
-                    <div className="ml-auto">score</div>
+  handleGameStart = async (e) => {
+    this.room = await this.client.joinOrCreate('game_room');
+
+    this.room.onMessage('joined', (player) => {
+      const message = `player ${player.username} joined.`;
+
+      const newChatState = this.state.chatState.unshift(message);
+      this.setChatState(newChatState);
+    });
+
+    this.room.onMessage('messages', (message) => {
+      const newChatState = this.state.chatState.unshift(message);
+      this.setChatState(newChatState);
+    });
+
+    this.room.onMessage('gameResult', (gameResult) => {
+      alert('Gameover!');
+
+      console.log(gameResult);
+    });
+
+    this.setGameState(true);
+  };
+
+  handleGameStop = (e) => {
+    if (this.room) {
+      this.room.send('exit');
+    }
+
+    this.setGameState(false);
+  };
+
+  render() {
+    const rankingList = [1, 2, 3];
+
+    return (
+      <div className="App justify-between h-screen">
+        {/* body */}
+        <div className="w-screen h-full bg-gray-300 flex">
+          {/* left-side container */}
+          <div className="w-7/12 block flex flex-col">
+            {/* match button & game board */}
+            <div className="w-full flex-grow relative">
+              {this.state.gameState ? (
+                /* game board */
+                <div className="w-full h-full flex flex-col justify-center items-center">
+                  <p className="h-auto mt-4 text-xl text-purple-500">
+                    Username vs Username
+                  </p>
+                  <div className="w-2/3 mt-2 mb-2 h-full flex justify-center">
+                    <GameBoard
+                      onGameEnd={this.handleGameStop}
+                      room={this.room}
+                    />
                   </div>
-                </li>
-              ))}
-            </ul>
+                </div>
+              ) : (
+                /* match button */
+                <div className="w-full h-full flex flex-col justify-center items-center">
+                  <p className="absolute top-0 mt-4 text-xl text-purple-500">
+                    welcome to online tic-tac-toe!
+                  </p>
+                  <button
+                    className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded text-4xl"
+                    onClick={this.handleGameStart}
+                  >
+                    PLAY NOW!
+                  </button>
+                  <div className="pt-3 text-2xl text-gray-700">0:00</div>
+                </div>
+              )}
+            </div>
+
+            {/* chatting */}
+            <div className="w-full px-4 py-2 block">
+              <ul className="h-48 flex-grow-0 flex flex-col-reverse overflow-auto">
+                {this.state.chatState.map((chat, key) => (
+                  <li key={key} className="w-full mx-2 flex flex-wrap">
+                    <div className="mr-4 text-base font-semibold">username</div>
+                    <div>{chat}</div>
+                  </li>
+                ))}
+              </ul>
+              <div className="mt-2 h-12">
+                <form action="" className="w-full h-full flex items-center">
+                  <input
+                    className="flex-grow h-8 px-2 rounded"
+                    type="text"
+                    placeholder="Input Text!"
+                  />
+                  <button
+                    className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-1 px-4 ml-4 rounded text-base"
+                    type="submit"
+                  >
+                    전송
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+          {/* right-side container */}
+          <div className="w-5/12 pt-8 pb-4 pl-8 pr-8">
+            {/* leaderboard */}
+            <div className="bg-gray-400 w-full h-full rounded flex flex-col">
+              <div className="flex justify-center pt-2 pb-4">
+                <h3 className="text-3xl text-purple-600">LEADERBOARDS</h3>
+              </div>
+              <ul className="flex-grow overflow-y-auto">
+                {rankingList.map((idx) => (
+                  <li key={idx} className="block w-full">
+                    <div className="flex bg-gray-500 rounded ml-6 mr-6 pl-4 pr-4 pt-1 pb-1 text-xl mb-2">
+                      <div className="w-3">{idx}</div>
+                      <div className="ml-5">username</div>
+                      <div className="ml-auto">score</div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,75 +1,67 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-const userId = '1';
-const opponentId = '2';
+class GameBoard extends React.Component {
+  state = {
+    markState: [],
+    turnCount: 0,
+  };
 
-function GameBoard({ onGameEnd }) {
-  const [mapState, setMapState] = useState(Array(100).fill(null));
-  const [turnCount, setTurnCount] = useState(0);
+  constructor({ room }) {
+    super();
+    this.room = room;
+  }
 
-  const handleBoardClick = (event) => {
-    const cellId = event.target.attributes.getNamedItem('cell-id').value;
-    let nowUserId = userId;
-
-    if (turnCount % 2 === 0) {
-      nowUserId = opponentId;
-    }
-
-    // 임시
-    // 게임 종료 상황 연출
-    if (turnCount === 6) {
-      handleGameEvent(null);
-    }
-
-    if (mapState[cellId]) {
-      return;
-    }
-
-    const newMapState = mapState.slice();
-
-    newMapState.splice(cellId, 1, {
-      userId: nowUserId,
-      turn: turnCount,
+  componentDidMount(props) {
+    this.room.onMessage('gameEvent', (mark) => {
+      this.setState({
+        markState: [...this.state.markState, mark],
+        turnCount: this.state.turnCount + 1,
+      });
     });
+  }
 
-    setMapState(newMapState);
-    setTurnCount(turnCount + 1);
+  reduceNumbers = () => {
+    const arr = [];
+
+    for (let i = 0; i < 100; i++) {
+      arr.push(i);
+    }
+
+    return arr;
   };
 
-  const handleGameEvent = (customEvent) => {
-    onGameEnd();
+  handleBoardClick = (idx) => {
+    this.room.send('mark', {
+      point: idx,
+    });
   };
 
-  useEffect(() => {
-    // on create
-    console.log('game start!');
-
-    return () => {
-      // on destory
-      console.log('end game');
-    };
-  }, []);
-
-  return (
-    <div className="h-full w-full bg-gray-400 grid grid-cols-10 grid-rows-10 rounded">
-      {[...Array(100).keys()].map((idx) => (
-        <div
-          key={idx}
-          cell-id={idx}
-          className="border flex justify-center items-center relative"
-          onClick={handleBoardClick}
-        >
-          {mapState[idx] ? (
-            <div className="absolute text-xl">
-              {mapState[idx].turn % 2 === 0 ? 'X' : 'O'}
-            </div>
-          ) : (
-            ''
-          )}
-        </div>
-      ))}
-    </div>
-  );
+  render() {
+    return (
+      <div className="h-full w-full bg-gray-400 grid grid-cols-10 grid-rows-10 rounded">
+        {this.reduceNumbers().map((idx) => (
+          <div
+            key={idx}
+            className="border flex justify-center items-center relative"
+            onClick={() => this.handleBoardClick(idx)}
+          >
+            {this.state.markState.find((mark) => mark.point === idx) ? (
+              <div className="absolute text-xl">
+                {this.state.markState.find((mark) => mark.point === idx)
+                  .turnNumber %
+                  2 ===
+                0
+                  ? 'X'
+                  : 'O'}
+              </div>
+            ) : (
+              ''
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default GameBoard;
